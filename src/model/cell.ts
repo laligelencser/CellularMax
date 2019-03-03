55720228883cad44fc94de501efe7645502c039e
laligelencser <gelen0440@gmail.com>
2019-03-03 12:13:42 +0100

    cell improvements

--- src/model/cell.ts
+++ src/model/cell.ts
@@ -1,9 +1,17 @@
-interface ICell {
-    connections: ICell[];
-    value: number;
-}
+export class Cell {
+    connections: Cell[] = [];
+    valuesInTime: number[] = [0];
+    numberOfValues: number = 2;
 
-export class Cell implements ICell {
-    connections: ICell[] = [];
-    value: number = 0;
+    sumConnections = (): number => {
+        let sum = 0;
+        for(let cell of this.connections) {
+            sum += cell.currentValue();
+        }
+        return sum;
+    }
+
+    currentValue = (): number => {
+        return this.valuesInTime[this.valuesInTime.length - 1];
+    }
 }
