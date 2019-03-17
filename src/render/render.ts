e0a9ad2fe9cc4e9f600da42c0af75fccaa10e7cd
laligelencser <gelen0440@gmail.com>
2019-03-17 11:05:51 +0100

    render based on cell value

--- src/render/render.ts
+++ src/render/render.ts
@@ -1,10 +1,16 @@
 import { Grid } from "../model/grid";
 import { Cell } from "../model/cell";
+import { AppState } from "../index";
 
-export const render = (ctx: CanvasRenderingContext2D, grid: Grid, settings: any = {}) => {
-    grid.iterate((cell: Cell, rowIndex: number, columnIndex: number) => {
-        const x = settings.rectWidth * columnIndex;
-        const y = settings.rectHeight * rowIndex;
-        ctx.fillRect(x, y, settings.rectWidth - 1, settings.rectHeight - 1);
+export const render = (appState: AppState) => {
+    const ctx = appState.canvas.getContext('2d');
+    appState.grid.iterate((cell: Cell, rowIndex: number, columnIndex: number) => {
+        const x = appState.rectWidth * columnIndex;
+        const y = appState.rectHeight * rowIndex;
+        if (cell.currentValue() === 1) {
+            ctx.fillRect(x, y, appState.rectWidth - 1, appState.rectHeight - 1);
+        } else {
+            ctx.strokeRect(x, y, appState.rectWidth - 1, appState.rectHeight - 1);
+        }
     });
 }
